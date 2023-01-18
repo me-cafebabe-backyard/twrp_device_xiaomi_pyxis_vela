9200eaed1c14b7879bcbb45bf723c7ca9b49b620
me-cafebabe <me_cafebabe@hotmail.com>
2023-01-18 17:52:01 +0800

    vela: Commonize `BOARD_USES_METADATA_PARTITION` flag

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -12,8 +12,5 @@ DEVICE_PATH := device/xiaomi/vela
 # Kernel
 TARGET_PREBUILT_KERNEL := device/xiaomi/kernel-sdm710/vela/Image.gz-dtb
 
-# Partitions
-BOARD_USES_METADATA_PARTITION := true
-
 # Recovery
 #TARGET_RECOVERY_DEVICE_DIRS += $(DEVICE_PATH)
