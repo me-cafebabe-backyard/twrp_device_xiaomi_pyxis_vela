9645d4b343fc5975845358a0d267f9ca31442b52
me-cafebabe <me_cafebabe@hotmail.com>
2023-01-18 17:54:25 +0800

    vela: Include correct prebuilt kernel and dtb/dtbo images

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -4,13 +4,17 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Kernel
+TARGET_PREBUILT_DTB := device/xiaomi/kernel-sdm710/vela/dtb.img
+
 # Inherit from common sdm710-common
 include device/xiaomi/sdm710-common/BoardConfigCommon.mk
 
 DEVICE_PATH := device/xiaomi/vela
 
 # Kernel
-TARGET_PREBUILT_KERNEL := device/xiaomi/kernel-sdm710/vela/Image.gz-dtb
+BOARD_PREBUILT_DTBOIMAGE := device/xiaomi/kernel-sdm710/vela/dtbo.img
+TARGET_PREBUILT_KERNEL := device/xiaomi/kernel-sdm710/vela/Image
 
 # Recovery
 #TARGET_RECOVERY_DEVICE_DIRS += $(DEVICE_PATH)
