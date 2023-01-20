7e64f57b16f8e1689f005464a44ee969cc82556d
me-cafebabe <me_cafebabe@hotmail.com>
2023-01-21 03:35:53 +0800

    pyxis_vela: Compare md5sum of board-id as fallback way for device determination

--- recovery/root/system/bin/determine-device.sh
+++ recovery/root/system/bin/determine-device.sh
@@ -2,7 +2,19 @@
 
 source /system/bin/mi710-utils.sh
 
-case "$(cat /sys/firmware/devicetree/base/firmware/device)" in
+DEVICE="$(cat /sys/firmware/devicetree/base/firmware/device)"
+if [ -z "$DEVICE" ]; then
+	case "$(md5sum /sys/firmware/devicetree/base/qcom,board-id)" in
+		"148192e49a5a8acefafb3671ee86fc49  /sys/firmware/devicetree/base/qcom,board-id")
+				DEVICE="pyxis"
+			;;
+		"50402368e7b1b8292fd7dcc6f8734ed4  /sys/firmware/devicetree/base/qcom,board-id")
+				DEVICE="vela"
+			;;
+	esac
+fi
+
+case "$DEVICE" in
 	"pyxis")
 		set_device_codename "pyxis"
 		set_device_model "Mi 9 Lite"
